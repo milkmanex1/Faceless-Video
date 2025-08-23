c63901e02ad1506ab64b72c9c9b25a47165de5b7
Gabriel <81763256+milkmanex1@users.noreply.github.com>
2025-08-23 16:15:39 +0800

    Update ingest.js

--- api/shotstack/ingest.js
+++ api/shotstack/ingest.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  console.log("Ingest request body:", req.body);
   // --- CORS headers ---
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
